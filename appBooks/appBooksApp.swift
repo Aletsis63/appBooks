bc543534514dfeaada1e38f4d9553570ce3f822e
Aletsis63 <cesar.ochoa.tapia10@gmail.com>
2023-06-09 22:58:42 -0600

    Initial Commit

---
+++ appBooks/appBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  appBooksApp.swift
+//  appBooks
+//
+//  Created by Cesar Alexis Ochoa Tapia on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct appBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
